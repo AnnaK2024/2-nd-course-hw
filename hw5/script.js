aa7776fce27e1afd7ee36e8ac8896f11c0b87ee2
annak2024 <kalininaann.pk@mail.ru>
2024-01-23 21:21:00 +0300

    Добавила 8 задание

--- hw5/script.js
+++ hw5/script.js
@@ -94,3 +94,19 @@ console.log(circle1.getPerimeter());
 console.log(circle2.getArea());
 
 console.log(circle2.getPerimeter());
+
+//*Задание 8
+function month() {
+    let num = Number(prompt('Пожалуйста, введите номер месяца.')); 
+      if (num === 1 || num === 2 || num === 12 ) {
+        console.log ('Этот месяц относится к зимнему периоду');
+      }else if (num === 3 || num === 4 || num === 5 ) {
+        console.log ('Этот месяц относится к весеннему периоду'); 
+      }else if (num === 6 || num === 7 || num === 8 ) {
+        console.log ('Этот месяц относится к летнему периоду');
+      }else if (num === 9 || num === 10 || num === 11 ) {
+        console.log ('Этот месяц относится к осеннему периоду');
+      }else if (num >= 13) {
+        console.log ('Некорректное число!');
+    }
+}
