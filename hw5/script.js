60625b98d071b4a511922ae1ed0b7c97eff19fc2
annak2024 <kalininaann.pk@mail.ru>
2024-01-23 19:35:34 +0300

    Дописала функцию

--- hw5/script.js
+++ hw5/script.js
@@ -94,3 +94,22 @@ console.log(circle1.getPerimeter());
 console.log(circle2.getArea());
 
 console.log(circle2.getPerimeter());
+
+//*Задание 8
+let num = Number(prompt('Пожалуйста, введите номер месяца.'));  
+function month(num) {
+      if (num === 1 || num === 2 || num === 12 ) {
+        return 'Этот месяц относится к зимнему периоду';
+    }else if (num === 3 || num === 4 || num === 5 ) {
+        return 'Этот месяц относится к весеннему периоду'; 
+    }else if (num === 6 || num === 7 || num === 8 ) {
+        return 'Этот месяц относится к летнему периоду';
+    }else if (num === 9 || num === 10 || num === 11 ) {
+        return 'Этот месяц относится к осеннему периоду';
+    }else if (num >= 13) {
+        return 'Такого месяца не существует!'
+    }
+};
+
+console.log(month(num));
+    
