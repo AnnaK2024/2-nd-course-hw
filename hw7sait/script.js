ab074e5e09083f1980455608cbab307ea3d8657b
annak2024 <kalininaann.pk@mail.ru>
2024-02-04 23:37:14 +0300

    Написала функциюк ко 2 игре

--- hw7sait/script.js
+++ hw7sait/script.js
@@ -1,26 +1,34 @@
 function month() {
   let num = Number(prompt('Пожалуйста, введите номер месяца.'));
       if (num === 1 || num === 2 || num === 12 ) {
-        return alert('Этот месяц относится к зимнему периоду');
+        return ('Этот месяц относится к зимнему периоду');
       }else if (num === 3 || num === 4 || num === 5 ) {
-        return alert('Этот месяц относится к весеннему периоду'); 
+        return ('Этот месяц относится к весеннему периоду'); 
       }else if (num === 6 || num === 7 || num === 8 ) {
-        return alert('Этот месяц относится к летнему периоду');
+        return ('Этот месяц относится к летнему периоду');
       }else if (num === 9 || num === 10 || num === 11 ) {
-        return alert('Этот месяц относится к осеннему периоду');
+        return ('Этот месяц относится к осеннему периоду');
       }else {
-        return alert('Некорректное число!');
+        return ('Некорректное число!');
       }
 }
 
-function words() {
-  let rem = prompt('Да Нет Незнаю');
-  if (rem === 'Да Нет Незнаю' ) {
-     return('Молодец! Верно!');
-  } else {
-     return('Попробуй еще раз');
-  }
+function gameWords() {
+  const fruits = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+  alert (fruits.sort(() => Math.random() - 0.5));
+  const questionOne = (prompt('Какое слово было первым?'));
+  questionOne = questionOne.toLocaleLowerCase();
+  const questionToo = (prompt('Какое слово было вторым?'));
+  questionToo = questionToo.toLocaleLowerCase();
+    if (fruits[0] === questionOne && fruits[fruits.length -1] === questionToo ) {
+      alert('Молодец! Верно!');
+    } else if (fruits[0] !==questionOne || fruits[fruits.length -1] !== questionToo) {
+      alert('Это было близко к победе!');
+    } else {
+      alertn('Не верно.Попробуй еще раз');
+    }
 }
+
 
 function riddle() {
   let rid = (prompt('Отгадай загадку - Висит груша, нельзя скушать'));
