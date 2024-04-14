ff6795bd14e510d391d1faf891edc2e9b5952afa
annak2024 <kalininaann.pk@mail.ru>
2024-04-15 01:11:37 +0300

    ДЗ Практика 2

---
+++ hwDraftComments_Модульный_JS_практика_2/mainP.js
@@ -0,0 +1,17 @@
+import { getTodos } from "./api.js";
+import { renderLogin } from "./renderLogin.js";
+import { renderTasks } from "./renderTasks.js";
+
+let tasks = [];
+
+const fetchAndRenderTasks = () => {
+  getTodos().then((responseData) => {
+    tasks = responseData.todos;
+    renderTasks(tasks, fetchAndRenderTasks);
+    return true;
+  });
+};
+
+renderLogin({fetchAndRenderTasks});
+// fetchAndRenderTasks();
+
