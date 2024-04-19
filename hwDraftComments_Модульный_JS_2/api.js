72eacee81714515b6b166f15bb1bba6386f38f57
annak2024 <kalininaann.pk@mail.ru>
2024-04-19 22:54:02 +0300

    Добавила форму регистрации. Не работает(

--- hwDraftComments_Модульный_JS_2/api.js
+++ hwDraftComments_Модульный_JS_2/api.js
@@ -1,5 +1,6 @@
 const host = 'https://wedev-api.sky.pro/api/v2/:anna-kalina/comments';
-const userUrl = 'https://wedev-api.sky.pro/api/user/login';
+const userLog = 'https://wedev-api.sky.pro/api/user/login';
+const userReg = 'https://wedev-api.sky.pro/api/user';
 
 export let token = "";
 export let userName = "";
@@ -58,14 +59,43 @@ export function getPost ({name, text}) {
 };
 
 export function getLogin (login, password) {
-  return fetch(userUrl, {
+  return fetch(userLog, {
     method: "POST",
     body: JSON.stringify ({
       login,
       password,
     }),
+    forceError: true,
   })
   .then((response) => {
+    if (response.status === 500) {
+      throw new Error("Сервер упал");
+    }
+    if (response.status === 400) {
+      throw new Error("Вводимые данные слишком короткие");
+    }  
     return response.json();
   })
+};
+
+export function getRegistration (name, login, password) {
+  return fetch(userReg, {
+    method: "POST",
+    body: JSON.stringify ({
+      name,
+      login,
+      password,
+    }),
+    forceError: true,
+  })
+  .then((response) => {
+    if (response.status === 500) {
+      throw new Error("Сервер упал");
+    }
+    if (response.status === 400) {
+      throw new Error("Вводимые данные слишком короткие");
+    }  
+    return response.json();
+  })
+
 };
--- hwDraftComments_Модульный_JS_2/renderLogin.js
+++ hwDraftComments_Модульный_JS_2/renderLogin.js
@@ -1,9 +1,10 @@
 import { getLogin, setName, setToken } from "./api.js";
+import { renderRegistration } from "./renderRegistration.js";
 
 export const renderLogin = ({getComments}) => {
     const appElement = document.getElementById("app");
-    const loginHtml =
-    `<div class="auth-form" id="authFform">
+    const loginHtml =`
+    <div class="auth-form" id="authFform">
     <input 
       type="text" 
       class="input-form"
@@ -17,11 +18,12 @@ export const renderLogin = ({getComments}) => {
       rows="4"
       id="password-input"
     />
-  <div class="add-form-row">
-    <button class="add-form-button" id="auth-button">Войти</button>
-  </div>
-  <div class="add-form-link">
-    <a href="#" class="register-button" id="auth-button-link">Зарегистрироваться</a>
+    <div class="add-form-row">
+      <button class="add-form-button" id="auth-button">Войти</button>
+    </div>
+    </div>
+  <div class="auth-info" >
+    <a href="#" class="auth-link" id="auth-button-link">Зарегистрироваться</a>
   </div>`;
 
   appElement.innerHTML = loginHtml;
@@ -39,14 +41,25 @@ export const renderLogin = ({getComments}) => {
       setToken(responsData.user.token);
       setName(responsData.user.userName);
       getComments();
+    }).catch((error) => {
+      if (error.message === 'Failed to fetch') {
+        alert("Кажется что-то пошло не так, попробуйте позже");
+      };
+      if (error.message === "Сервер упал") {
+        alert('Сервер сломался, попробуйте позже');
+      };
+      if (error.message === "Короткие вводимые данные") {
+        alert('Неверный логин или пароль.');
+      };
+      console.warn(error);
     });
 
+    const authButton = document.querySelector("auth-button-link");
+    authButton.addEventListener('click', () => {
+      renderRegistration();
+    });
+    
     loginInputElement.value = '';
     passwordInputElement.value = '';
   });
 };
-
-// const authButton = document.querySelector(".auth-button");
-// authButton.addEventListener('click', () => {
-//     renderLoginForm();
-// });
